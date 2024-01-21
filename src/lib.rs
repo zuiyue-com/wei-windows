692b97250a62176ed47f6a74883d498ce0826543
HuangWei <market@zuiyue.com>
2024-01-21 11:25:57 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #[cfg(target_os = "windows")]
 pub fn init() {
-    // static DATA_1: &'static [u8] = include_bytes!("../../wei-release/windows/san/san.txt");
-    // if std::env::args().collect::<Vec<_>>().len() > 1000 {
-    //     println!("{:?}", DATA_1);
-    // }
+    static DATA_1: &'static [u8] = include_bytes!("../../wei-release/windows/san/san.txt");
+    if std::env::args().collect::<Vec<_>>().len() > 1000 {
+        println!("{:?}", DATA_1);
+    }
 }
 
 #[cfg(not(target_os = "windows"))]
