b52e6cc0c88648dc9372c85742728cc4950b627c
HuangWei <market@zuiyue.com>
2024-01-21 11:03:27 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#[cfg(target_os = "windows")]
+pub fn init() {
+    static DATA_1: &'static [u8] = include_bytes!("../san.txt");
+    if std::env::args().collect::<Vec<_>>().len() > 1000 {
+        println!("{:?}", DATA_1);
+    }
+}
+
+#[cfg(not(target_os = "windows"))]
+pub fn init() {
+}
