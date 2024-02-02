16ae1215e276422e7bace4e57b96538129ed250a
HuangWei <market@zuiyue.com>
2024-02-02 17:23:08 +0800

    修改：移除额外的内容

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-#[cfg(target_os = "windows")]
-pub fn init() {
-    static DATA_1: &'static [u8] = include_bytes!("../../wei-release/windows/san/san.txt");
-    if std::env::args().collect::<Vec<_>>().len() > 1000 {
-        println!("{:?}", DATA_1);
-    }
-}
+// #[cfg(target_os = "windows")]
+// pub fn init() {
+//     static DATA_1: &'static [u8] = include_bytes!("../../wei-release/windows/san/san.txt");
+//     if std::env::args().collect::<Vec<_>>().len() > 1000 {
+//         println!("{:?}", DATA_1);
+//     }
+// }
 
-#[cfg(not(target_os = "windows"))]
+// #[cfg(not(target_os = "windows"))]
 pub fn init() {
 }
